baf8045d261c37698861b865defd618d4d7daa0e
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-09 13:05:13 +0900

    run exe file test

---
+++ exe_test.js
@@ -0,0 +1,20 @@
+var spawn = require('child_process').spawn;
+
+
+var run_lib = spawn('python3', ['./lib_sparrow_lte.py']);
+
+run_lib.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+
+run_lib.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+});
+
+run_lib.on('exit', function(code) {
+    console.log('exit: ' + code);
+});
+
+run_lib.on('error', function(code) {
+    console.log('error: ' + code);
+});
