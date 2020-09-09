c1c4fd9c16aaa0385705fd34b42fa95780e52a61
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-09 13:07:07 +0900

    run exe test

--- exe_test.js
+++ exe_test.js
@@ -1,20 +1,27 @@
 var spawn = require('child_process').spawn;
 
+// var run_lib = spawn('python3', ['./lib_sparrow_lte.exe']);
 
-var run_lib = spawn('python3', ['./lib_sparrow_lte.py']);
+// run_lib.stdout.on('data', function(data) {
+//     console.log('stdout: ' + data);
+// });
 
-run_lib.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
-});
+// run_lib.stderr.on('data', function(data) {
+//     console.log('stderr: ' + data);
+// });
 
-run_lib.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
-});
+// run_lib.on('exit', function(code) {
+//     console.log('exit: ' + code);
+// });
 
-run_lib.on('exit', function(code) {
-    console.log('exit: ' + code);
-});
+// run_lib.on('error', function(code) {
+//     console.log('error: ' + code);
+// });
 
-run_lib.on('error', function(code) {
-    console.log('error: ' + code);
+const { execFile } = require('child_process');
+const child = execFile('./lib_sparrow_lte.exe', ['/dev/ttyUSB1', '115200'], (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
 });
